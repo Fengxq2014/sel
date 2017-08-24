73bc124615de459ec849149f68f10844f7702459
Fengxq2014 <zxcasd555888@gmail.com>
2017-08-24 16:23:06 +0800

    update null string

--- models/user.go
+++ models/user.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"database/sql"
 	"time"
 
 	db "github.com/Fengxq2014/sel/database"
@@ -19,8 +20,11 @@ type User struct {
 
 // GetUserByOpenid 通过微信微信身份标识获取客户信息
 func (u *User) GetUserByOpenid() (user User, err error) {
-	err = db.SqlDB.QueryRow("SELECT a.user_id,a.phone_number,a.name,a.role,a.head_portrait,b.child_id FROM user a left join uc_relation b on b.user_id=a.user_id where a.openid = ?", u.Openid).Scan(&user.User_id, &user.Phone_number, &user.Name, &user.Role, &user.Head_portrait, &user.Child_id)
-
+	var child_id sql.NullString
+	err = db.SqlDB.QueryRow("SELECT a.user_id,a.phone_number,a.name,a.role,a.head_portrait,b.child_id FROM user a left join uc_relation b on b.user_id=a.user_id where a.openid = ?", u.Openid).Scan(&user.User_id, &user.Phone_number, &user.Name, &user.Role, &user.Head_portrait, &child_id)
+	if child_id.Valid {
+		user.Child_id = child_id.String
+	}
 	if err != nil {
 		return user, err
 	}
